3ae463f078147a35dbaa9561c611c083f16ad836
Lope Emano <lopegwapo@gmail.com>
2013-03-15 00:02:30 +0800

    initial commit

---
+++ src/com/lopefied/hellonfc/MainActivity.java
@@ -0,0 +1,142 @@
+
+package com.lopefied.hellonfc;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final String HELLO_WORLD = "Hello Philippine Android Developer Community!";
+
+    Tag mTag;
+    NfcAdapter adapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] {
+                tagDetected
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        adapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        Intent intent = getIntent();
+//        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(getIntent().getAction())) {
+//            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//            mTag = tag;
+//            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+//            if (rawMsgs != null) {
+//                NdefRecord relayRecord = ((NdefMessage) rawMsgs[0]).getRecords()[0];
+//                String nfcData = new String(relayRecord.getPayload());
+//                Toast.makeText(this, "NFC Data : " + nfcData, Toast.LENGTH_SHORT).show();
+//            }
+//        }
+        adapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            mTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                NdefRecord relayRecord = ((NdefMessage) rawMsgs[0]).getRecords()[0];
+                String nfcData = new String(relayRecord.getPayload());
+                // Display the data on the tag
+                Toast.makeText(this, "NFC Data : " + nfcData, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        if (mTag != null)
+            try {
+                write(HELLO_WORLD, mTag);
+                Toast.makeText(this, "Successfully wrote message to nfc tag : " + HELLO_WORLD, Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (FormatException e) {
+                e.printStackTrace();
+            }
+    }
+
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+        // create the message in according with the standard
+        String lang = "en";
+        byte[] textBytes = text.getBytes();
+        byte[] langBytes = lang.getBytes("US-ASCII");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+
+        byte[] payload = new byte[1 + langLength + textLength];
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1, langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT,
+                new byte[0], payload);
+        return recordNFC;
+    }
+
+    private void write(String text, Tag tag) throws IOException, FormatException {
+        NdefRecord[] records = {
+                createRecord(text)
+        };
+        NdefMessage message = new NdefMessage(records);
+        Ndef ndef = Ndef.get(tag);
+        if (ndef != null) {
+            ndef.connect();
+            ndef.writeNdefMessage(message);
+            ndef.close();
+        } else {
+            NdefFormatable format = NdefFormatable.get(tag);
+            if (format != null) {
+                format.connect();
+                format.format(message);
+                format.close();
+            }
+        }
+    }
+}
